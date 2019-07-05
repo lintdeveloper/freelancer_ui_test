8a98120e6dcb4939c702384e0feb3342e6ef8269
Musa Musa <musabrillz@gmail.com>
2019-07-05 15:53:28 +0100

    Created a boilerplate code for bottom navigation bar

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -8,7 +8,31 @@ class SettingsScreen extends StatelessWidget{
         centerTitle: true,
         title: Text('Screen not found'),
       ),
-      body: Center(),
+      body: Center(
+        child: Text("Hello"),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () { },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+        backgroundColor: Color(0xFF0047CC),
+        elevation: 4.0,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(icon: Icon(Icons.menu), onPressed: () {},),
+            IconButton(icon: Icon(Icons.search), onPressed: () {},),
+            IconButton(icon: Icon(Icons.menu), onPressed: () {},),
+            IconButton(icon: Icon(Icons.search), onPressed: () {},),
+          ],
+        ),
+        shape: CircularNotchedRectangle(),
+        color: Colors.white,
+      ),
     );
   }
 
