84dd9657d2f369583ba6d155c922263409d8ab7e
Musa Musa <musabrillz@gmail.com>
2019-07-06 08:36:18 +0100

    Refractored the card boilerplate code

--- lib/widgets/profile_card.dart
+++ lib/widgets/profile_card.dart
@@ -24,32 +24,54 @@ class ProfileCard extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Image.asset("assets/images/ricardo.png", height: 40, width: 50,),
+                Image.asset(
+                  "assets/images/ricardo.png",
+                  height: 40,
+                  width: 80,
+                ),
                 SizedBox(
                   height: 5,
                 ),
                 Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    profile.userName,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      profile.userName,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Color.fromRGBO(4, 44, 92, 1),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Image.asset("assets/images/verified.png")
+                  ],
+                ),
+                Text(profile.userEmail,
                     style: TextStyle(
-                      fontSize: 18,
-                      color: Color.fromRGBO(4, 44, 92, 1),
-                      fontWeight: FontWeight.bold,
+                        color: Colors.grey.shade400,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 14.0)),
+                SizedBox(height: 15.0),
+                Container(
+                  width: _media.width,
+                  color: Color.fromRGBO(248, 249, 249, 1),
+                  margin: EdgeInsets.only(top: 2.0, bottom: 8.0),
+                  child: Container(
+                    margin: EdgeInsets.only(top: 8.0, bottom: 8.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Image.asset("assets/images/badge1.png"),
+                        Image.asset("assets/images/badge1.png"),
+                        Image.asset("assets/images/badge1.png"),
+                        Image.asset("assets/images/badge1.png")
+                      ],
                     ),
                   ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Image.asset("assets/images/verified.png")
-                ],
-                  
-              ),
-                Text(
-                  profile.userEmail,
-
-                  style: TextStyle(color: Colors.grey.shade400, fontWeight: FontWeight.w700, fontSize: 14.0 )
                 )
               ],
             ),
